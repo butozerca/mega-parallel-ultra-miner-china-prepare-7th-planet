5f29b7ecf20b387d5bcb0463ad83757be9d4fba3
phantom10111 <phantom10111@gmail.com>
2015-02-21 16:56:56 +0100

    Main weryfikuje hashe

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include<cstdio>
 #include<chrono>
+#include<climits>
+#include<cstdio>
+#include<cstring>
 #include<fstream>
 #include<iostream>
 #include<string>
@@ -7,17 +9,58 @@
 
 #include "Cpu_miner.hpp"
 #include "Gpu_miner.hpp"
+#include "sha256.hpp"
 
 const std::string file_in = "input.txt";
 
 
-int main () {
+void confirm_nonce(const char *data, int nonce, int difficulty)
+{
+	unsigned char data2[80], hash[32], hash2[32];
+
+	memcpy(data2, data, 76);
+	*reinterpret_cast<int *>(data2 + 76) = nonce;
+
+	SHA256 sha;
+	sha.init();
+	sha.update(data2, 80);
+	sha.final(hash);
+
+	sha.init();
+	sha.update(hash, 32);
+	sha.final(hash2);
+
+	char hash_str[65];
+	for(int i = 0; i < 32; ++i)
+		sprintf(hash_str + 2 * i, "%02x", hash2[i]);
+	hash_str[64] = 0;
+
+	std::cout << "Policzony hash " << hash_str << std::endl;
+
+	--difficulty;
+	char cmp_str[65];
+
+	for(int i = 0; i < difficulty / 4; ++i)
+		cmp_str[i] = '0';
+
+	cmp_str[difficulty / 4] = '0' + (8 >> (difficulty % 4));
+	cmp_str[difficulty / 4 + 1] = 0;
+
+	if(strcmp(hash_str, cmp_str) < 0)
+		std::cout << "Dobry hash" << std::endl;
+	else
+		std::cout << "Zły hash" << std::endl;
+}
+
+int main()
+{
 
     std::ifstream input(file_in);
     
     std::vector<std::string> in;
 
-    while (input.good()) {
+    while(input.good())
+    {
         std::string s;
         input >> s;
         in.push_back(s);
@@ -32,8 +75,8 @@ int main () {
     Cpu_miner cpu_miner;
     Gpu_miner gpu_miner;
 
-    int difficulty = 4;
-    int max_nonce = 100000;
+    int difficulty = 20;
+    int max_nonce = INT_MAX;
 
 
     for (auto &s: in) {
@@ -41,16 +84,18 @@ int main () {
         auto start = std::chrono::high_resolution_clock::now();
         int res = cpu_miner.mine(s.c_str(), 0, max_nonce, difficulty);
         auto finish = std::chrono::high_resolution_clock::now();
-        printf("cpu res=%d\n", res);
+        printf("cpu nonce=%d\n", res);
         auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish-start);
         std::cout << "cpu mining took " << microseconds.count() << "µs\n";
+        confirm_nonce(s.c_str(), res, difficulty);
     
         start = std::chrono::high_resolution_clock::now();
         res = gpu_miner.mine(s.c_str(), 0, max_nonce, difficulty);
         finish = std::chrono::high_resolution_clock::now();
-        printf("gpu res=%d\n", res);
+        printf("gpu nonce=%d\n", res);
         microseconds = std::chrono::duration_cast<std::chrono::microseconds>(finish-start);
         std::cout << "GPU mining took " << microseconds.count() << "µs\n";
+        confirm_nonce(s.c_str(), res, difficulty);
     }
 
     return 0;
